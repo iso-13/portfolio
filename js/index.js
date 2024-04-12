8b0f3ff8aaa126eebb2a3624dce285da9d029f4c
sarah <islam-hussein@outlook.com>
2024-04-12 19:32:33 +0300

    nav update

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const closeMenuElm = document.querySelector(".close-menu-btn");
 const sideMenuElm = document.querySelector(".side-menu");
 const overlayElm = document.querySelector(".overlay");
 const scrollersElms = document.querySelectorAll(".scroller-logo");
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
 
 function addAnimation() {
   const innerScroller = document.querySelector(".inner-scroller");
@@ -39,3 +41,26 @@ function closeMenu() {
   sideMenuElm.style.left = "-281px";
   overlayElm.style.display = "none";
 }
+
+function updateActiveClass() {
+  sections.forEach((sec) => {
+    const secTop = sec.offsetTop;
+    const secHeight = sec.offsetHeight;
+    const secBottom = secTop + secHeight;
+
+    const offset = 60; // navbar height
+
+    if (window.scrollY + offset >= secTop && scrollY < secBottom) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        closeMenu();
+      });
+      const matchingLink = document.querySelector(`a[href="#${sec.id}"]`);
+      matchingLink.classList.add("active");
+    }
+  });
+}
+
+updateActiveClass();
+
+window.addEventListener("scroll", updateActiveClass);
